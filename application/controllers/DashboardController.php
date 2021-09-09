44e91af4bd5e31b2a69ce490ca7648395b274316
AmelliaKurniaty <56011358+AmelliaKurniaty@users.noreply.github.com>
2021-09-09 19:43:07 +0700

    first commit

---
+++ application/controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class DashboardController extends CI_Controller {
+    
+        public function index()
+        {
+            $this->load->view('dashboard');
+        }
+    
+    }
+    
+    /* End of file DashboardController.php */
+    
+
+?>
---
+++ application/views/dashboard.php
@@ -0,0 +1,1 @@
+<h3>Halaman Utama</h3>
